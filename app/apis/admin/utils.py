bf3b55983a225759ab2347cfcac335cd6fe803dd
Ana Karina <anakarinarivera2010@gmail.com>
2025-11-15 17:42:55 -0500

    Update utils.py
    Level 5 Solucionado

--- app/apis/admin/utils.py
+++ app/apis/admin/utils.py
@@ -2,7 +2,14 @@ import subprocess
 
 
 def get_disk_usage(parameters: str):
-    command = "df -h " + parameters
+    allowed = ["-h", "-a", "-T"] # parameters
+
+    args = parameters.split()
+    for a in args:
+        if a not in allowed:
+            raise Exception("Invalid parameter")
+
+    command = ["df"] + args
 
     try:
         result = subprocess.run(
